4e30b59d6b8cf50bd0de91a567762dd775b80b62
shuhui wang <76734406+2508756189@users.noreply.github.com>
2023-04-28 17:05:14 +0800

    wsh-3个界面接口

---
+++ IFirstSelectRepairmanService.java
@@ -0,0 +1,70 @@
+package com.ruoyi.apartment.service;
+
+import java.util.List;
+import com.ruoyi.apartment.domain.FirstSelectRepairman;
+import com.ruoyi.common.core.domain.entity.SysUser;
+
+/**
+ * 一次派单Service接口
+ *
+ * @author ljc
+ * @date 2023-04-24
+ */
+public interface IFirstSelectRepairmanService
+{
+    /**
+     * 查询一次派单
+     *
+     * @param repairId 一次派单主键
+     * @return 一次派单
+     */
+    public FirstSelectRepairman selectFirstSelectRepairmanByRepairId(Long repairId);
+
+    /**
+     * 查询一次派单列表
+     *
+     * @param firstSelectRepairman 一次派单
+     * @return 一次派单集合
+     */
+    public List<FirstSelectRepairman> selectFirstSelectRepairmanList(FirstSelectRepairman firstSelectRepairman);
+
+    /**
+     * 新增一次派单
+     *
+     * @param firstSelectRepairman 一次派单
+     * @return 结果
+     */
+    public int insertFirstSelectRepairman(FirstSelectRepairman firstSelectRepairman);
+
+    /**
+     * 修改一次派单
+     *
+     * @param firstSelectRepairman 一次派单
+     * @return 结果
+     */
+    public int updateFirstSelectRepairman(FirstSelectRepairman firstSelectRepairman);
+
+    /**
+     * 批量删除一次派单
+     *
+     * @param repairIds 需要删除的一次派单主键集合
+     * @return 结果
+     */
+    public int deleteFirstSelectRepairmanByRepairIds(Long[] repairIds);
+
+    /**
+     * 删除一次派单信息
+     *
+     * @param repairId 一次派单主键
+     * @return 结果
+     */
+    public int deleteFirstSelectRepairmanByRepairId(Long repairId);
+
+    /**
+     * 根据角色Id查询用户信息
+     *
+     * @param roleId 角色Id
+     * @return 结果
+     */
+    public List<SysUser> selectUserByRoleId(Long roleId);
+}
---
+++ ManagerOpinionController.java
@@ -0,0 +1,119 @@
+package com.ruoyi.apartment.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ruoyi.apartment.service.IFirstSelectRepairmanService;
+import com.ruoyi.common.core.domain.entity.SysUser;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.apartment.domain.ManagerOpinion;
+import com.ruoyi.apartment.service.IManagerOpinionService;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.page.TableDataInfo;
+
+/**
+ * 领导审批Controller
+ *
+ * @author ljc
+ * @date 2023-04-24
+ */
+@RestController
+@RequestMapping("/apartment/managerOpinion")
+public class ManagerOpinionController extends BaseController
+{
+    @Autowired
+    private IManagerOpinionService managerOpinionService;
+
+    @Autowired
+    private IFirstSelectRepairmanService firstSelectRepairmanService;
+
+    /**
+     * 查询领导审批列表
+     */
+    @PreAuthorize("@ss.hasPermi('apartment:managerOpinion:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(ManagerOpinion managerOpinion)
+    {
+        startPage();
+        List<ManagerOpinion> list = managerOpinionService.selectManagerOpinionList(managerOpinion);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出领导审批列表
+     */
+    @PreAuthorize("@ss.hasPermi('apartment:managerOpinion:export')")
+    @Log(title = "领导审批", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, ManagerOpinion managerOpinion)
+    {
+        List<ManagerOpinion> list = managerOpinionService.selectManagerOpinionList(managerOpinion);
+        ExcelUtil<ManagerOpinion> util = new ExcelUtil<ManagerOpinion>(ManagerOpinion.class);
+        util.exportExcel(response, list, "领导审批数据");
+    }
+
+    /**
+     * 获取领导审批详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('apartment:managerOpinion:query')")
+    @GetMapping(value = "/{repairId}")
+    public AjaxResult getInfo(@PathVariable("repairId") Long repairId)
+    {
+        return success(managerOpinionService.selectManagerOpinionByRepairId(repairId));
+    }
+
+    /**
+     * 新增领导审批
+     */
+    @PreAuthorize("@ss.hasPermi('apartment:managerOpinion:add')")
+    @Log(title = "领导审批", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody ManagerOpinion managerOpinion)
+    {
+        return toAjax(managerOpinionService.insertManagerOpinion(managerOpinion));
+    }
+
+    /**
+     * 修改领导审批
+     */
+    @PreAuthorize("@ss.hasPermi('apartment:managerOpinion:edit')")
+    @Log(title = "领导审批", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody ManagerOpinion managerOpinion)
+    {
+        return toAjax(managerOpinionService.updateManagerOpinion(managerOpinion));
+    }
+
+    /**
+     * 删除领导审批
+     */
+    @PreAuthorize("@ss.hasPermi('apartment:managerOpinion:remove')")
+    @Log(title = "领导审批", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{repairIds}")
+    public AjaxResult remove(@PathVariable Long[] repairIds)
+    {
+        return toAjax(managerOpinionService.deleteManagerOpinionByRepairIds(repairIds));
+    }
+
+    /**
+     * 根据角色ID查询用户信息
+     */
+//    @PreAuthorize("@ss.hasPermi('apartment:firstSelectRepairman:selectUserByRoleId')")
+    @GetMapping("/selectUserByRoleId/{roleId}")
+    public List<SysUser> selectUserByRoleId(@PathVariable("roleId") Long roleId) {
+        return firstSelectRepairmanService.selectUserByRoleId(roleId);
+    }
+}
